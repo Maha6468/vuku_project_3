2a93adf3cfece446ac8c685db61c51820c4897e0
Maha6468 <motherma7633@gmail.com>
2026-02-07 01:05:12 +0600

    last update

--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -1,29 +1,33 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+//
+// class Splash_Screen extends StatelessWidget {
+//   const Splash_Screen({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Color(0xFF54BB52),
+//       body: Stack(
+//         children: [
+//           Center(
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 Image.asset("assets/images/Vector.png",height: 119,width: 119,),
+//                 Image.asset("assets/images/Vector (1).png",height: 43,width: 145,)
+//               ],
+//             ),
+//           ),
+//           Positioned(
+//             bottom: 80,left: 0,right: 0,top: 600,
+//               child:Image.asset("assets/images/Loading Pro.png",height: 50,width: 50,),
+//
+//           )
+//         ],
+//       ),
+//
+//     );
+//   }
+// }
 
-class Splash_Screen extends StatelessWidget {
-  const Splash_Screen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFF54BB52),
-      body:Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Center(
-            child: Column(
-              children: [
-                SizedBox(height: 200,),
-                Image.asset("assets/images/Vector.png",height: 119,width: 119,),
-                Image.asset("assets/images/Vector (1).png",height: 43,width: 145,),
-              ],
-            ),
-          ),
-          Image.asset("assets/images/Loading Pro.png"),
-
-
-        ],
-      )
-    );
-  }
-}

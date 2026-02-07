fb541edce21136d465c3189b44c22468c8730575
Maha6468 <motherma7633@gmail.com>
2026-02-08 01:45:32 +0600

    last update

--- lib/Screens/onboarding_1.dart
+++ lib/Screens/onboarding_1.dart
@@ -1,126 +1,102 @@
-import 'package:curved_labeled_navigation_bar/curved_navigation_bar.dart';
-import 'package:curved_labeled_navigation_bar/curved_navigation_bar_item.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class onbording_1 extends StatefulWidget {
-  const onbording_1({super.key});
-
-  @override
-  State<onbording_1> createState() => _onbording_1State();
-}
-
-class _onbording_1State extends State<onbording_1> {
+class Onboarding_1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:  Color(0xFFE5F4E2),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              flex: 4,
-              child: Center(
-                child: Image.asset(
-                  "assets/images/Frame.png",
-                  height: 260,
-                ),
-              ),
+      //backgroundColor: Color(0xffe8f5e9),
+      backgroundColor: Color(0xFFD7EAD6),
+      body: Column(
+        children: [
+          const SizedBox(height: 60),
+          Expanded(
+            flex: 4,
+            child: Center(
+              child: Image.asset("assets/images/Frame.png", height: 260),
             ),
-            Expanded(
-              flex: 5,
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 40),
-                child: Container(
+          ),
+
+          Expanded(
+            flex: 4,
+            child: Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Container(
+                  height: 300,
                   width: double.infinity,
-                 // height: 407,
-                  padding: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-                  decoration:  BoxDecoration(
+                  padding: EdgeInsets.symmetric(horizontal: 24, vertical: 50),
+                  decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.vertical(
                       top: Radius.circular(30),
+                      bottom: Radius.circular(30),
                     ),
                   ),
                   child: Column(
                     children: [
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          _dot(true),
-                          _dot(false),
-                          _dot(false),
-                        ],
+                        children: [_dot(true), _dot(false), _dot(false)],
                       ),
-
                       SizedBox(height: 20),
-                      Text("DISCOVER THE JOY OF\nREADING",
+                      Text(
+                        "DISCOVER THE JOY OF\nREADING",
                         textAlign: TextAlign.center,
                         style: TextStyle(
                           fontSize: 22,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                      SizedBox(height: 14),
-                      Text("Unleash a world of stories tailored to your child’s age and reading level. "
-                            "Vuku makes reading fun from the very first page.",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.grey,
-                          height: 1.4,
+                      SizedBox(height: 10),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Text(
+                          "Unleash a world of stories tailored to your child's age and reading level."
+                          "Vuku makes reading fun from the very first page",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.grey, fontSize: 14),
                         ),
                       ),
-                      Spacer(),
-
-                      // Container(
-                      //   height: 70,
-                      //   width: 70,
-                      //   decoration: BoxDecoration(
-                      //     color: const Color(0xFF63C25D),
-                      //     shape: BoxShape.circle,
-                      //   ),
-                      //   child: const Icon(
-                      //     Icons.arrow_forward_ios,
-                      //     color: Colors.white,
-                      //     size: 28,
-                      //   ),
-                      // ),
-
                     ],
                   ),
                 ),
-              ),
+
+                Align(
+                  alignment: Alignment(-.01, .7),
+                  child: Container(
+                    height: 60,
+                    width: 60,
+                    decoration: BoxDecoration(
+                      color: Color(0xFF54BB52),
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.double_arrow_outlined,
+                        size: 25,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-
-
-      bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: Colors.amber,
-        items: [
-          CurvedNavigationBarItem(
-            child: Icon(Icons.double_arrow_rounded)
-          )
+          ),
         ],
       ),
-
-
-
-
     );
   }
+}
 
-  /// dot widget
-  Widget _dot(bool isActive) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 4),
-      height: 8,
-      width: isActive ? 14 : 8,
-      decoration: BoxDecoration(
-        color: isActive ? Colors.green : Colors.grey.shade300,
-        borderRadius: BorderRadius.circular(10),
-      ),
-    );
-  }
+Widget _dot(bool isActive) {
+  return Container(
+    margin: const EdgeInsets.symmetric(horizontal: 4),
+    height: 8,
+    width: isActive ? 14 : 8,
+    decoration: BoxDecoration(
+      color: isActive ? Colors.green : Colors.grey.shade300,
+      borderRadius: BorderRadius.circular(10),
+    ),
+  );
 }

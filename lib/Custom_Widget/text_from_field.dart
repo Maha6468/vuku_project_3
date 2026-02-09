1a374e0fd2bfc5aaa492cbf01c2f47d34fe4ac79
Maha6468 <motherma7633@gmail.com>
2026-02-09 20:16:03 +0600

    last update

---
+++ lib/Custom_Widget/text_from_field.dart
@@ -0,0 +1,60 @@
+
+import 'package:flutter/material.dart';
+
+class Text_Form_Field extends StatelessWidget {
+  final String? lablText;
+  final String? hinText;
+  final bool filled;
+  final Color? fillColor;
+  final IconData? preIcon;
+  final IconData? sufIcon;
+  const Text_Form_Field({super.key, required this.filled, this.fillColor, this.preIcon, this.sufIcon, this.hinText, this.lablText});
+
+  @override
+  Widget build(BuildContext context) {
+    return  SizedBox(
+      width: 330,
+      height: 50,
+      child: TextFormField(
+        decoration: InputDecoration(
+          // labelText:label,
+          labelText: lablText != null ? lablText : null,
+          //hintText: "Maha",
+          hintText: hinText != null ? hinText : null,
+          filled:filled,
+          fillColor: fillColor,
+          // prefixIcon: Icon(Icons.add_alert),
+          prefixIcon: preIcon != null ? Icon(preIcon) : null,
+          // suffixIcon: Icon(Icons.add),
+          suffixIcon: sufIcon!=null? Icon(sufIcon):null,
+
+          enabledBorder:OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide(
+              color: Colors.grey
+            )
+          ),
+
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            //borderSide: BorderSide.none,
+            borderSide: BorderSide(
+              color: Colors.grey
+            )
+          ),
+
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide(
+              color: Colors.grey
+            )
+          )
+
+
+
+
+        ),
+      ),
+    );
+  }
+}

2ae878e835a0e6df17d5f42fb9742c6415b6b6b1
Maha6468 <motherma7633@gmail.com>
2026-02-09 18:58:49 +0600

    last update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vuku_project_3/Screens/Splash&Onboarding%20flow/onboarding_2.dart';
 import 'package:vuku_project_3/Screens/Splash&Onboarding%20flow/splash_screen.dart';
 
 import 'Screens/Splash&Onboarding flow/onboarding_1.dart';
@@ -17,7 +18,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Onboarding_1(),
+      //home: Onboarding_1(),
+      home: Onboarding_2(),
+
     );
   }
 }

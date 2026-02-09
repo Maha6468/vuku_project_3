fb72c9e9bd008eb539a46da8cad351f5e7004c9b
Maha6468 <motherma7633@gmail.com>
2026-02-09 21:01:29 +0600

    last update

--- lib/Screens/log_in.dart
+++ lib/Screens/log_in.dart
@@ -23,16 +23,16 @@ class _Log_InState extends State<Log_In> {
             Text("Use Credentials to access your account",style: TextStyle(fontSize:16,color: Colors.grey),),
             SizedBox(height: 30,),
             Text_Form_Field(
+              text: "E-mail",
               filled: true,
               hinText:"Enter your Email",
-
             ),
-            SizedBox(height: 15,),
+            SizedBox(height: 20,),
             Text_Form_Field(
+              text: "Password",
               filled: true,
               hinText:"Enter Password",
               sufIcon:(Icons.remove_red_eye_outlined),
-
             ),
 
 
